b856f71d8a00e67391ffe8011bb05cb50afe7c7b
Bjarki Eldon <beldon11@gmail.com>
2026-07-19 16:34:47 +0200

    header file for  deltanull-poissondirichlet-varyingN.nw
    header file for  deltanull-poissondirichlet-varyingN.nw

---
+++ deltanull-poissondirichlet-varyingN.h
@@ -0,0 +1,13 @@
+constexpr unsigned int SAMPLE_SIZE = 20;
+constexpr double EXPERIMENTS = 1.e2;
+/*
+constexpr double ALPHA = 0.99;
+constexpr double KAPPA = 2.0 ;
+constexpr double CEPS = 10. ;
+constexpr double RHO = 25. ;
+constexpr double MINF = (2. + (1 + pow(2., 1-KAPPA)/(KAPPA - 1)))/2. ;
+constexpr double BKAPPA = pow(2,KAPPA)*(KAPPA - 2)*(KAPPA - 1);
+constexpr double AKAPPA = ((KAPPA+2) + pow(KAPPA,2))/2.;
+constexpr double CKAPPA = (KAPPA > 2 ? AKAPPA / BKAPPA : 1.) * 2/pow(MINF,2.) ;
+constexpr double EXPERIMENTS = 1e2 ;
+*/
